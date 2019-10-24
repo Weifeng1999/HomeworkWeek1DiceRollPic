5fc285227981f1516e7f5deab53a18e6d9a03f53
Weifeng1999 <ooiweifeng@outlook.com>
2019-10-24 11:21:52 +0800

    a

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -19,10 +19,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        /*Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()*/
 
-        val randomInt = Random().nextInt(6) + 1
+        for (i in 1..3) dice(i)
+
+        /*val randomInt = Random().nextInt(6) + 1
         val randomInt2 = Random().nextInt(6) + 1
         val randomInt3 = Random().nextInt(6) + 1
 
@@ -59,6 +59,33 @@ class MainActivity : AppCompatActivity() {
 
         diceImage.setImageResource(drawableResource)
         diceImage2.setImageResource(drawableResource2)
-        diceImage3.setImageResource(drawableResource3)
+        diceImage3.setImageResource(drawableResource3)*/
+    }
+
+    private fun dice(i: Int){
+
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        when (i) {
+            1 -> {
+                val diceImage: ImageView = findViewById(R.id.dice_image)
+                diceImage.setImageResource(drawableResource)
+            }
+            2 -> {
+                val diceImage2: ImageView = findViewById(R.id.dice_image2)
+                diceImage2.setImageResource(drawableResource)
+            }
+            else -> {
+                val diceImage3: ImageView = findViewById(R.id.dice_image3)
+                diceImage3.setImageResource(drawableResource)
+            }
+        }
     }
 }
